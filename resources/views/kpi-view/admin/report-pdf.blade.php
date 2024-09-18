d13b20109d5a7af30077073fc598e1861bf5a99b
Haekal Sastradilaga <haekalboedoet@gmail.com>
2024-09-18 23:12:31 +0700

    asd

--- resources/views/kpi-view/admin/report-pdf.blade.php
+++ resources/views/kpi-view/admin/report-pdf.blade.php
@@ -6,11 +6,15 @@
     <title>KPI Report</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"> --}}
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <style>
         @page{
             margin-top: 80px;
+        }
+        /* Apply Poppins font to the entire document */
+        body {
+            font-family: 'Poppins', sans-serif;
         }
 
         header{
