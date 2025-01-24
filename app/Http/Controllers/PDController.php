03f681b93ad7e0afa0a4f33639834aa8e8165070
Haekal Sastradilaga <haekalboedoet@gmail.com>
2025-01-24 15:41:51 +0700

    asdasd

--- app/Http/Controllers/PDController.php
+++ app/Http/Controllers/PDController.php
@@ -1531,7 +1531,7 @@ class PDController extends Controller
                 $qrCodeData = QrCode::format('png')
                     ->size(200)
                     ->merge('/storage/app/MTC.png', 0.3) // Merge with a 30% size of the QR code
-                    ->errorCorrection('L') // Use high error correction level
+                    ->errorCorrection('M') // Use high error correction level
                     ->generate(route('validate-certificate', $encryptedId));
 
                 // Embed the QR Code in the template
